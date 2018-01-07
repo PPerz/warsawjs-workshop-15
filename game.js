b32c2826dfe6f9b0e9d5fdb4302e65877e10dbec
PPerz <piotr.perz.90@gmail.com>
2018-01-07 12:20:41 +0100

    Step 3 - switch player

--- game.js
+++ game.js
@@ -1,19 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+
+
+    var playerClasses = {
+
+        'playerA': 'red',
+        'playerB': 'blue'
+    };
+
+    var currentPlayer;
+
+
     initGame();
 
     function initGame() {
         var fields = document.querySelectorAll('.board > div');
 
-
+        currentPlayer = 'playerA'
         fields.forEach(field => {
             field.addEventListener('click', fieldClickHandler)
         });
     }
 
     function fieldClickHandler() {
-        this.classList.add("red");
+        var playerClass = playerClasses[currentPlayer];
+        this.classList.add(playerClass);
         console.log('clicked', this);
+
+        if (currentPlayer === 'playerA') {
+            currentPlayer = 'playerB';
+        } else {
+            currentPlayer = 'playerA';
+        }
     }
 
 });
